57a07535bc98e8936b7faa42d3d3701682970beb
Tijani Oluwafemi <olutimedia@gmail.com>
2019-05-20 13:22:37 +0100

    feat(ui): user gets all questions [delivers #166115804]

---
+++ ui/js/dashboard.js
@@ -0,0 +1,46 @@
+let questionCard = document.getElementById('question-card');
+let alertTitle = document.querySelector('.alertTitle');
+
+const url = 'http://stack-o-lite.herokuapp.com/api/v1/'
+
+
+// get all parties
+window.onload = () => {
+  
+  
+  class displayData {
+    // Make an HTTP GET Request 
+  
+    async getAll() {
+        const questionResponse = await fetch(`${url}questions`);
+        const allQuestions = await questionResponse.json();
+        const questionData = allQuestions.data;
+  //      console.log(questionData);
+    //    console.log(allQuestions);
+        return {
+            questionData
+        }
+      }
+  }
+
+
+const showQuestions = new displayData;
+//console.log(showQuestions)
+showQuestions.getAll()
+  .then(data => {
+    console.log(data)
+    questionCard.innerHTML = data.questionData.questions.map((question) => {
+      //console.log(question);
+      let output = '';
+      return output += `<div class="card">
+                        <h4 id='questionTitle'>${question.questionTitle}</h4>
+                        <p id='questionsDescription'>${question.questionDescription}</p>
+                        <p id='qDate'>${question.date}</p>
+                        </div>`
+    })
+  })
+  /*
+  .catch(err => {
+    alertTitle.innerText = 'An error has occured';
+  });*/
+  }
