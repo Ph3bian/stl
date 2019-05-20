8a694e5be7ecc431b682d1b325c348c071da2402
Phebian Chukwurah <user@Phebian.local>
2019-05-20 13:24:23 +0100

    Feature(ui): Completed sign in and Sign up To do validations

---
+++ ui/js/sign-in.js
@@ -0,0 +1,20 @@
+let data = {};
+const submitForm=()=>{
+data.email = document.getElementById('email').value;
+data.password = document.getElementById('password').value;
+const Url= 'https://stack-o-lite.herokuapp.com/api/v1/auth/login';
+ axios.post(Url, data, {
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    }
+    )
+.then(response=>{
+  console.log(response);
+  if(response.data.status== 200){
+    window.location.href='./dashboard.html'
+  }
+}).catch(error=>{
+  console.log(error);
+})
+}
---
+++ ui/js/sign-up.js
@@ -0,0 +1,26 @@
+let data = {};
+
+ console.log("got heresirrr!!!")
+const submitForm=()=>{
+  console.log("got here!!!")
+data.firstName = document.getElementById('firstName').value;
+data.lastName = document.getElementById('lastName').value;
+data.email = document.getElementById('email').value;
+data.password = document.getElementById('password').value;
+data.confirmPassword = document.getElementById('comfirmPassword').value;
+const Url= 'https://stack-o-lite.herokuapp.com/api/v1/auth/signup';
+ axios.post(Url, data, {
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    }
+    )
+.then(response=>{
+  console.log(response);
+  if(response.data.status== 201){
+    window.location.href='/dashboard.html'
+  }
+}).catch(error=>{
+  console.log(error);
+})
+}
